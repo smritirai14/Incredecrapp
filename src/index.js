d3e2c45a2312fcbdc7acfa9f29a13f181acc2554
smritirai14 <smritirai145@gmail.com>
2021-06-26 00:06:14 +0530

    first task

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const [num, setNum] = useState(0);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const incNum = () => {
+    setNum (num +1);
+  };
+
+  const decNum = () => {
+    setNum(num -2);
+  };
+
+  const addNum = () => {
+    setNum(num +5);
+  };
+
+  const resetNum = () => {
+    setNum(0);
+  };
+
+  return (
+    <>
+      <div className="main_div">
+      <div className ="center_div">
+      <h1> {num} </h1>
+      <div className="btn_div">
+         <button onClick={incNum} > Increment By 1 </button>
+         <button onClick={decNum} > Decrement By 2</button> 
+         <button onClick={addNum} > Increment By 5 </button>
+         <button onClick={resetNum} > Reset </button>
+    </div>
+    </div>
+    </div>
+</>
+
+  );
+}
+export default App;
